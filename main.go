ee8e786d5fcb70d8d1c2b6537996b35fddf439b4
Abhinav Kumar <sharma.abhinavkr@gmail.com>
2026-03-30 09:40:17 +0530

    Serving file server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	servMux := http.NewServeMux()
+	servMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Handler: servMux,
 		Addr:    ":8080",
